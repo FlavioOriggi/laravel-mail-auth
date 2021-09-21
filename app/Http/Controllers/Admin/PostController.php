3bf8fe291a2aca3927a5cae3a4e02ab93430ddee
Flavio <flavio.origgi@outlook.it>
2021-09-21 22:29:02 +0200

    add Img

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Post;
+use App\Tag;
+use App\Category;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        $categories = Category::All();
+        return view('admin.posts.index', compact('posts','categories'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('admin.posts.create', compact('categories','tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:60',
+            'content' => 'required',
+            'category_id' => 'nullable|exists:categories,id',
+            'image' => 'nullable|image'
+
+
+
+
+        ]);
+
+        $data = $request->all();
+
+        $new_post = new Post();
+
+        $slug = Str::slug($data['title'],'-');
+        $slug_base = $slug;
+        
+        $slug_presente = Post::where('slug', $slug)->first();
+        $contatore = 1;
+
+        while($slug_presente){
+            $slug = $slug_base . '-' .$contatore ;
+
+            $slug_presente = Post::where('slug', $slug)->first();
+
+            $contatore++;
+        }
+
+        $new_post->slug = $slug;
+
+        if (array_key_exists('image',$data)) {
+            //salviamo la nostra immagine e recuperiamo il path
+            $cover_path = Storage::put('covers', $data['image']);  
+
+            //salviamo nella colonna della tabella posts l'immagine con il suo percorso
+            $data['cover'] = $cover_path;
+        }
+
+        $new_post->fill($data);
+         
+        $new_post->save();  
+
+        if(array_key_exists('tags',$data)){
+            $new_post->tags()->attach($data['tags']);  
+        }       
+        
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $post = Post::where('slug',$slug)->first();        
+        return view('admin.posts.show', compact('post'));
+    }
+
+    // collegamento con id
+    // public function show(Post $post)
+    // {        
+    //     return view('admin.posts.show', compact ('post'));
+    // }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        // dd($categories);
+        return view('admin.posts.edit', compact('post', 'categories','tags'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|max:60',
+            'content' => 'required',
+            'category_id' => 'nullable|exists:categories,id',
+            'image' => 'nullable|image'
+
+        ]);
+
+        $data = $request->all();
+        if($data['title'] != $post->title){
+
+            $slug = Str::slug($data['title'], '-');
+            $slug_base = $slug;
+
+            $slug_presente = Post::where('slug', $slug)->first();
+            $contatore = 1;
+
+            while($slug_presente){
+
+                $slug = $slug_base . '-' . $contatore;
+                $slug_presente = Post::where('slug', $slug)->first();
+                $contatore++;
+            }
+
+            $data['slug'] = $slug;
+        }
+
+        if (array_key_exists('image',$data)) {
+            //salviamo la nostra immagine e recuperiamo il path
+            $cover_path = Storage::put('covers', $data['image']);  
+
+            
+            //salviamo nella colonna della tabella posts l'immagine con il suo percorso
+            $data['cover'] = $cover_path;
+        }
+
+        $post->update($data);
+
+        if(array_key_exists('tags', $data)) {
+            $post->tags()->sync($data['tags']);
+        }
+
+        return redirect()->route('admin.posts.index')->with('update', 'Modificato correttamente l\'id ' .$post->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        Storage::delete($post->cover);
+        $post->delete();
+
+        // $post->tags()->detach();        
+        return redirect()->route('admin.posts.index')->with('delete', 'Hai cancellato l\'id ' .$post->id);
+    }
+}
---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>            
+        @endif
+
+        <form action="{{ route('admin.posts.update', $post->id) }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
+            <div class="mb-3">
+                <label for="titolo" class="form-label">Titolo</label>
+                <input type="text" name="title" class="form-control" id="titolo" value="{{ old('title', $post->title) }}">    
+            </div>
+
+            <div class="mb-3">
+                <label for="category" class="form-label">Categoria</label>
+                <select class="form-control" name="category_id" id="category">
+                    <option value="">-- Seleziona una categoria --</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" 
+                            @if ($category->id == old('category_id', $post->category_id)) selected 
+                            @endif>  
+                            {{ $category->name }}</option>                        
+                    @endforeach
+                </select>                
+            </div>
+
+            <div class="mb-3">
+                @if ($post->cover)
+                    <img src="{{ asset('storage/' . $post->cover)}}" alt="">                    
+                @endif
+            </div>
+
+            <div class="mb-3">
+                <label for="img" class="form-label">Immagine</label>
+                <input id="img" type="file" name="image" class="form-control-file
+                @error('image')
+                    is-invalid                                       
+                @enderror">
+                @error('image')
+                    <div class="alert alert-danger">{{ $message }} </div>                    
+                @enderror
+
+            </div>
+
+            <div class="mb-3">
+                <label for="desc" class="form-label">Descrizione</label>
+                <textarea name="content" id="desc" cols="30" rows="10" class="form-control">{{ old('content', $post->content) }}</textarea>  
+            </div>
+
+            <div>
+                <h4>Tag</h4>
+                    @foreach ($tags as $tag)
+                        <div class="d-inline-block  mx-1">
+                            <input id="tag{{ $loop->iteration }}" type="checkbox" value="{{ $tag->id }}" 
+                                @if ( !$errors->any() && $post->tags->contains( $tag->id ))
+                                    checked                                      
+                                @elseif(in_array($tag->id, old('tags', [])))                                        
+                                    checked
+                                @endif
+                            name="tags[]">
+                            <label for="tag{{ $loop->iteration }}" class="form-label">{{ $tag->name }}</label>
+                        </div>                            
+                    @endforeach
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        
+    </div>    
+@endsection
